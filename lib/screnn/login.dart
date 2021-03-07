5eeafa2a3ed5e28ffb573c3a5d3be64a0f3a22e7
thiago magno <magnothiago750@gmail.com>
2021-03-07 17:50:08 -0300

    Adiconando tela de login

---
+++ lib/screnn/login.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import '../components/button_login.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(
+          top: 80,
+          left: 40,
+          right: 40,
+        ),
+        color: Colors.white,
+        child: ListView(
+          children: [
+            SizedBox(
+              width: 240,
+              height: 240,
+              //child: Image.asset('assets/Logo_alfa.png'),
+              child: Image.asset('assets/alfa.png'),
+              //child: Image.asset('assets/alfa_novo.png'),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Button(Colors.red, Colors.white, 'LOGIN'),
+            SizedBox(
+              width: 30,
+            ),
+            Button(Colors.red, Colors.white, 'CRIAR CONTA'),
+          ],
+        ),
+      ),
+    );
+  }
+}
