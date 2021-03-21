2c04d5b1171e054ad7267b0b1e71ff1a102cf3bb
Brenneguer <wbrenneguer07@gmail.com>
2021-03-20 22:17:13 -0300

    Atualizando rotas

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,5 @@
-import 'package:alfa_banck/screnn/formulario_cadastro_user.dart';
-import 'package:alfa_banck/screnn/login.dart';
-import 'package:alfa_banck/screnn/tela_inicial.dart';
-import 'package:alfa_banck/screnn/tela_principal.dart';
+import 'package:alfa_banck/app.dart';
 import 'package:flutter/material.dart';
-import 'screnn/lista_transferencia.dart';
 
-void main() => runApp(App());
+void main() => runApp(AlfaBank());
 
-class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-          primaryColor: Colors.red,
-          accentColor: Colors.redAccent,
-          buttonTheme: ButtonThemeData(
-            buttonColor: Colors.redAccent,
-          )),
-      home: TelaInicial(),
-    );
-  }
-}
--- lib/src/screnn/tela_principal.dart
+++ lib/src/screnn/tela_principal.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class TelaPrincipal extends StatelessWidget {
+  static const String routeName = "tela_principal";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
