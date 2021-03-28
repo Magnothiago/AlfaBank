8d9a058c2ec3e88e1b28fa6f501e8e7224b97629
Brenneguer <wbrenneguer07@gmail.com>
2021-03-28 16:38:54 -0300

    autenticando login de usuário, permitindo somente usuário previamente cadastrado.

--- lib/src/resources/authentication_resources.dart
+++ lib/src/resources/authentication_resources.dart
@@ -1,3 +1,4 @@
+import 'package:alfa_banck/src/blocs/authentication/authentication_bloc.dart';
 import 'package:alfa_banck/src/modules/usuario.dart';
 import 'package:alfa_banck/src/resources/repository/persistationDb.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -6,41 +7,34 @@ import 'package:flutter/services.dart';
 class AuthenticationResources {
   //implementei firebase Auth para que nessa classe eu tenha acesso a todos os metodos do firebase
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-
   //Recebe no seu fluxo objetos do tipo do firebase user, contem todas as informações do usuario atual logado
   Stream<User> get onAuthStateChange => _firebaseAuth.authStateChanges();
 
   Future<int> cadastrarComEmailAndSenha(Usuario usuario) async {
     try {
-      print(
-          "authentication_resources line: 16 ${usuario.email}, ${usuario.cpf}");
       bool salvar = await persistenceService.adicionarUsuario(usuario);
       if (salvar) {
-        await _firebaseAuth.createUserWithEmailAndPassword(
-            email: usuario.email, password: usuario.senha);
+        await _firebaseAuth.createUserWithEmailAndPassword(email: usuario.email, password: usuario.senha);
         return 1;
       } else {
-        print(
-            'Ocorreu algum problema ao cadastrar o usuário, criar mensagem para exibição.');
+        print('Ocorreu algum problema ao cadastrar o usuário, criar mensagem para exibição.');
         return 0;
       }
     } on PlatformException catch (e) {
-      print(
-          'authentication_resources line: 22\n Platform Exception: Authentication ${e.toString()}');
+      print('authentication_resources line: 22\n Platform Exception: Authentication ${e.toString()}');
       return -1;
     } catch (e) {
-      print(
-          'authentication_resources line: 22\n Authenticaton ${e.toString()}');
+      print('authentication_resources line: 22\n Authenticaton ${e.toString()}');
       return -2;
     }
   }
 
-  Future<int> loginComEmailAndSenha(String email, String senha) async {
+
+
+  Future<int> loginComEmailAndSenha(Usuario usuario, String email, String
+  senha) async {
     try {
-      print(
-          'authentication_resources line: 32\ntentando: $email, senha: $senha');
-      await _firebaseAuth.signInWithEmailAndPassword(
-          email: email, password: senha);
+      await _firebaseAuth.signInWithEmailAndPassword(email: email, password: senha);
       return 1;
     } on PlatformException catch (e) {
       print(
@@ -57,6 +51,18 @@ class AuthenticationResources {
     _firebaseAuth.signOut();
   }
 
-//TODO ESCREVER METODO LINHA 40 authentication_resources.dart
+  Future<void> setUserDisplayName(String displayName) async {
+    onAuthStateChange.listen((event) {
+      print(event);
+      event.updateProfile(displayName: displayName);
+   });
+  }
 
+  void alterarUsuarioLogado(Usuario usuario) async {
+    authenticationBloc.mudarEmail(usuario.email);
+    authenticationBloc.mudarNomeUsuario(usuario.nome);
+    authenticationBloc.mudarIsLogado(true);
+    authenticationBloc.mudarCpf(usuario.cpf);
+    await setUserDisplayName(usuario.nome);
+  }
 }
