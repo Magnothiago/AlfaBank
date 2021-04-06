c80991c362e598851e70db78dcf26fe48d8de925
Brenneguer <wbrenneguer07@gmail.com>
2021-04-06 03:03:48 -0300

    Exibindo dados de usuário no sistema.

--- lib/src/resources/authentication_resources.dart
+++ lib/src/resources/authentication_resources.dart
@@ -1,3 +1,4 @@
+import 'package:alfa_banck/src/blocs/authentication/authentication_bloc.dart';
 import 'package:alfa_banck/src/modules/usuario.dart';
 import 'package:alfa_banck/src/resources/repository/persistationDb.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -17,6 +18,7 @@ class AuthenticationResources {
             .email, password: usuario.senha);
         await _firebaseAuth.currentUser.updateProfile(displayName: usuario
             .nome);
+        await atualizarDados(usuario);
         return 1;
       } else {
         print('Ocorreu algum problema ao cadastrar o usuário, criar mensagem para exibição.');
@@ -36,6 +38,7 @@ class AuthenticationResources {
     try {
       await _firebaseAuth.signInWithEmailAndPassword(email: email, password: senha);
       await _firebaseAuth.currentUser.updateProfile(displayName: usuario.nome);
+      await atualizarDados(usuario);
       return 1;
     } on PlatformException catch (e) {
       print(
@@ -50,5 +53,15 @@ class AuthenticationResources {
 
   Future<void> get sair async {
     _firebaseAuth.signOut();
+  }
+
+  Future<Usuario> atualizarDados(Usuario usuario) async {
+    authenticationBloc.mudarEmail(usuario.email);
+    authenticationBloc.mudarNomeUsuario(usuario.nome);
+    authenticationBloc.mudarCpf(usuario.cpf);
+    authenticationBloc.mudarTelefone(usuario.telefone);
+    authenticationBloc.mudarSenha(usuario.senha);
+    authenticationBloc.mudarUsuario(usuario);
+    return usuario;
   }
 }
