da65b05e2733d1c40575b9e99ab9988418c203d1
Thiago Magno <magnothiago750@gmail.com>
2021-06-09 22:23:06 -0300

    tirando debug banner

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -13,6 +13,7 @@ class AlfaBank extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primaryColor: Colors.red,
           accentColor: Colors.redAccent,
